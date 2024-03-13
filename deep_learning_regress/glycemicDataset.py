0748018a07a30ab695fe2b3acdf790b444e00a89
mhl34 <67287623+mhl34@users.noreply.github.com>
2024-03-13 18:14:19 -0500

    Update glycemicDataset.py

--- deep_learning_regress/glycemicDataset.py
+++ deep_learning_regress/glycemicDataset.py
@@ -25,23 +25,28 @@ class glycemicDataset(Dataset):
     
     def __getitem__(self, index):
         sample = random.choice(self.samples)
+        # get the different samples
         glucoseSample = self.glucose[sample]
         edaSample = self.eda[sample]
         hrSample = self.hr[sample]
         tempSample = self.temp[sample]
         accSample = self.acc[sample]
         hba1cSample = self.hba1c[sample]
-        # glucStart = random.randint(0,len(glucoseSample) - self.seq_length - 1)
+        
+        # get the start indices for different sequences
         glucStart = index
         edaStart = glucStart * self.pp5vals.eda
         hrStart = glucStart * self.pp5vals.hr
         tempStart = glucStart * self.pp5vals.temp
         accStart = glucStart * self.pp5vals.acc
-        glucTruth = True in np.isnan(glucoseSample[glucStart: glucStart + self.seq_length]) or len(glucoseSample[glucStart: glucStart + self.seq_length]) != self.seq_length
-        edaTruth = True in np.isnan(edaSample[edaStart: edaStart + self.seq_length * self.pp5vals.eda]) or len(edaSample[edaStart: edaStart + self.seq_length * self.pp5vals.eda]) != self.seq_length * self.pp5vals.eda
-        hrTruth = True in np.isnan(hrSample[hrStart: hrStart + self.seq_length * self.pp5vals.hr]) or len(hrSample[hrStart: hrStart + self.seq_length * self.pp5vals.hr]) != self.seq_length * self.pp5vals.hr
-        tempTruth = True in np.isnan(tempSample[tempStart: tempStart + self.seq_length * self.pp5vals.temp]) or len(tempSample[tempStart: tempStart + self.seq_length * self.pp5vals.temp]) != self.seq_length * self.pp5vals.temp
-        accTruth = True in np.isnan(accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]) or len(accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]) != self.seq_length * self.pp5vals.acc
+        
+        # truth value for gluc, eda, hr, temp, or acc
+        glucTruth = self.truthCheck(glucoseSample, glucStart, "glucose")
+        edaTruth = self.truthCheck(edaSample, edaStart, "eda")
+        hrTruth = self.truthCheck(hrSample, hrStart, "hr")
+        tempTruth = self.truthCheck(tempSample, tempStart, "temp")
+        accTruth = self.truthCheck(accSample, accStart, "acc")
+        
         while glucTruth or edaTruth or hrTruth or tempTruth or accTruth:
         # while glucTruth or edaTruth or hrTruth or tempTruth:
             glucStart = random.randint(0,len(glucoseSample) - self.seq_length - 1)
@@ -49,26 +54,34 @@ class glycemicDataset(Dataset):
             hrStart = glucStart * self.pp5vals.hr
             tempStart = glucStart * self.pp5vals.temp
             accStart = glucStart * self.pp5vals.acc
-            glucTruth = True in np.isnan(glucoseSample[glucStart: glucStart + self.seq_length]) or len(glucoseSample[glucStart: glucStart + self.seq_length]) != self.seq_length
-            edaTruth = True in np.isnan(edaSample[edaStart: edaStart + self.seq_length * self.pp5vals.eda]).astype(int) or len(edaSample[edaStart: edaStart + self.seq_length * self.pp5vals.eda]) != self.seq_length * self.pp5vals.eda
-            hrTruth = True in np.isnan(hrSample[hrStart: hrStart + self.seq_length * self.pp5vals.hr]).astype(int) or len(hrSample[hrStart: hrStart + self.seq_length * self.pp5vals.hr]) != self.seq_length * self.pp5vals.hr
-            tempTruth = True in np.isnan(tempSample[tempStart: tempStart + self.seq_length * self.pp5vals.temp]).astype(int) or len(tempSample[tempStart: tempStart + self.seq_length * self.pp5vals.temp]) != self.seq_length * self.pp5vals.temp
-            accTruth = True in np.isnan(accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]).astype(int) or len(accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]) != self.seq_length * self.pp5vals.acc
-        glucoseSec = glucoseSample[glucStart: glucStart + self.seq_length]
+            glucTruth = self.truthCheck(glucoseSample, glucStart, "glucose")
+            edaTruth = self.truthCheck(edaSample, edaStart, "eda")
+            hrTruth = self.truthCheck(hrSample, hrStart, "hr")
+            tempTruth = self.truthCheck(tempSample, tempStart, "temp")
+            accTruth = self.truthCheck(accSample, accStart, "acc")
+            
+        glucosePastSec = glucoseSample[glucStart: glucStart + self.seq_length]
+        glucoseSec = glucoseSample[glucStart + self.seq_length + 1: glucStart + 2 * self.seq_length + 1]
         edaSec = edaSample[edaStart: edaStart + self.seq_length * self.pp5vals.eda]
         hrSec = hrSample[hrStart: hrStart + self.seq_length * self.pp5vals.hr]
         tempSec = tempSample[tempStart: tempStart + self.seq_length * self.pp5vals.temp]
         accSec = accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]
-        glucStats = createGlucStats(glucoseSec)
+        # glucStats = createGlucStats(glucoseSec)
+        
         # create averages across sequence length
         edaMean = np.array(list(map(np.mean, np.array_split(edaSec, self.seq_length))))
         hrMean = np.array(list(map(np.mean, np.array_split(hrSec, self.seq_length))))
         tempMean = np.array(list(map(np.mean, np.array_split(tempSec, self.seq_length))))
         accMean = np.array(list(map(np.mean, np.array_split(accSec, self.seq_length))))
+        glucPastMean = np.array(list(map(np.mean, np.array_split(glucosePastSec, self.seq_length))))
         glucMean = np.array(list(map(np.mean, np.array_split(glucoseSec, self.seq_length))))
+        
+        # normalize if needed
         if self.normalize:
-            return (sample, self.normalizeFn(edaMean), self.normalizeFn(hrMean), self.normalizeFn(tempMean), self.normalizeFn(accMean), self.normalizeFn(glucMean), glucStats)
-        return (sample, edaMean, hrMean, tempMean, accMean, glucMean, glucStats)
+            return (sample, self.normalizeFn(edaMean), self.normalizeFn(hrMean), self.normalizeFn(tempMean), self.normalizeFn(accMean), self.normalizeFn(glucPastMean), self.normalizeFn(glucMean))
+        
+        #return normal
+        return (sample, edaMean, hrMean, tempMean, accMean, glucPastMean, glucMean)
     
     def normalizeFn(self, data, eps = 1e-5):
         data = data[~np.isnan(data)]
@@ -76,3 +89,9 @@ class glycemicDataset(Dataset):
         mean = np.mean(data)
         scaled_data = (data - mean) / np.sqrt(var + eps)
         return scaled_data
+    
+    def truthCheck(self, sample_array, sample_start, sample_type):
+        if sample_type == "glucose":
+            return True in np.isnan(sample_array[sample_start: sample_start + 2 * self.seq_length + 1]) or len(sample_array[sample_start: sample_start  + 2 * self.seq_length + 1]) != 2 * self.seq_length + 1
+        pp5val_dict = {"eda": self.pp5vals.eda, "hr": self.pp5vals.hr, "temp": self.pp5vals.temp, "acc": self.pp5vals.acc}
+        return True in np.isnan(sample_array[sample_start: sample_start + 2 * self.seq_length + 1]) or len(sample_array[sample_start: sample_start  + 2 * self.seq_length + 1]) != 2 * self.seq_length + 1
