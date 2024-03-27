14bdb095b19309b3953df6bce390e13d13b7cd53
mhl34 <67287623+mhl34@users.noreply.github.com>
2024-03-27 00:30:09 -0400

    Update FeatureDataset.py

--- feature_select_regress/FeatureDataset.py
+++ feature_select_regress/FeatureDataset.py
@@ -83,22 +83,20 @@ class FeatureDataset(Dataset):
 
         # normalize if needed
         if self.normalize:
-            return (sample, self.normalizeFn(accSample, accMean), self.normalizeFn(sugarSample, sugarMean), self.normalizeFn(carbSample, carbMean), self.normalizeFn(minSample, minMean), self.normalizeFn(hba1cSample, hba1cMean), self.normalizeFn(glucoseSample, glucPastMean), self.normalizeFn(glucoseSample, glucMean))
+            return (sample, self.normalizeFn(accMean), self.normalizeFn(sugarMean), self.normalizeFn(carbMean), self.normalizeFn(minMean), self.normalizeFn(hba1cMean), self.normalizeFn(glucPastMean), self.normalizeFn(glucMean))
         
-
-
         #return non-normalized outputs
         return (sample, accMean, sugarMean, carbMean, minMean, hba1cMean, glucPastMean, glucMean)
     
-    def normalizeFn(self, sample_data, data, eps = 1e-5):
+    def normalizeFn(self, data, eps = 1e-5):
         data = data[~np.isnan(data)]
-        var = np.var(sample_data)
-        mean = np.mean(sample_data)
-        scaled_data = (data - mean) / np.sqrt(var + eps)
+        data_min = np.min(data)
+        data_max = np.max(data)
+        scaled_data = (data - data_min) / (data_max - data_min + eps)
         return scaled_data
     
     def truthCheck(self, sample_array, sample_start, sample_type):
         if sample_type == "glucose":
             return True in np.isnan(sample_array[sample_start: sample_start + 2 * self.seq_length + 1]) or len(sample_array[sample_start: sample_start  + 2 * self.seq_length + 1]) != 2 * self.seq_length + 1
         pp5val_dict = {"eda": self.pp5vals.eda, "hr": self.pp5vals.hr, "temp": self.pp5vals.temp, "acc": self.pp5vals.acc, "gluc_other": 1}
-        return True in np.isnan(sample_array[sample_start: sample_start + self.seq_length + 1]) or len(sample_array[sample_start: sample_start + self.seq_length + 1]) != self.seq_length + 1
+        return True in np.isnan(sample_array[sample_start: sample_start + pp5val_dict[sample_type] * self.seq_length + 1]) or len(sample_array[sample_start: sample_start + pp5val_dict[sample_type] * self.seq_length + 1]) != pp5val_dict[sample_type] * self.seq_length + 1
