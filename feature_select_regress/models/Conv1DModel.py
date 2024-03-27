eac74198cfbf3b2b163770ceae67a451c605bd33
mhl34 <67287623+mhl34@users.noreply.github.com>
2024-03-27 01:16:40 -0400

    Update Conv1DModel.py

--- feature_select_regress/models/Conv1DModel.py
+++ feature_select_regress/models/Conv1DModel.py
@@ -1,35 +1,44 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from models.ChannelFC import ChannelFC
+import random
 
-class Conv1DModel(nn.Module):
-    def __init__(self, num_features, dropout_p = 0, normalize = False, seq_len = 28, dtype = torch.float):
-        super(Conv1DModel, self).__init__()
+class LstmModel(nn.Module):
+    def __init__(self, num_features, input_size, hidden_size, num_layers = 8, batch_first = True, dropout_p = 0.5, dtype = torch.float64, seq_len = 28):
+        super(LstmModel, self).__init__()
         self.num_features = num_features
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+        self.dropout_p = dropout_p
+        self.dtype = dtype
         self.seq_len = seq_len
-        # input: seq_len x (num_features + 1)
-        # num_features and then previous glucose
-        # 3 channels for each of the different modalities
-        # output: seq_len x 8
-        self.conv1 = nn.Conv1d(in_channels = self.num_features, out_channels = 28, kernel_size = 7, stride = 1, padding = 3).to(dtype)
-        # input: seq_len x 8
-        # output: seq_len x 16
-        self.conv2 = nn.Conv1d(in_channels = 28, out_channels = 48, kernel_size = 5, stride = 1, padding = 2).to(dtype)
-        # input: seq_len x 16
-        # output: seq_len x 64
-        self.conv3 = nn.Conv1d(in_channels = 48, out_channels = 84, kernel_size = 3, stride = 1, padding = 1).to(dtype)
-        self.dropout = nn.Dropout(dropout_p).to(dtype)
-        self.fc1 = nn.Linear(84 * self.seq_len, 64).to(dtype)
-        self.fc2 = nn.Linear(64, self.seq_len).to(dtype)
-        self.normalize = normalize
-    
+        self.lstm_encoder = nn.LSTM(input_size = self.input_size * self.num_features, hidden_size = self.hidden_size, num_layers = self.num_layers, batch_first = self.batch_first, dropout = self.dropout_p, dtype = self.dtype)
+        # self.lstm_decoder = nn.LSTM(input_size = self.hidden_size, hidden_size = self.input_size, num_layers = self.num_layers, batch_first = self.batch_first, dropout = self.dropout_p, dtype = self.dtype)
+        self.fc1 = nn.Linear(self.hidden_size, 64, dtype = self.dtype)
+        self.fc2 = nn.Linear(64, 32, dtype = self.dtype)
+        self.fc3 = nn.Linear(32, self.input_size, dtype = self.dtype)
+        # self.ssl = True
+        self.mask_len = 7
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.decoder = nn.Identity()
+
     def forward(self, x):
-        x = torch.tensor(x.clone().detach().requires_grad_(True), dtype=self.conv1.weight.dtype)
-        out = F.relu(self.conv1(x))
-        out = F.relu(self.conv2(out))
-        out = F.relu(self.conv3(out))
-        out = out.view(out.size(0), -1)
+        x = x.reshape(x.size(0), -1).to(self.dtype)
+        out, _ = self.lstm_encoder(x)
+        # out, _ = self.lstm_decoder(out)
+        out = out.reshape(out.size(0), -1).to(self.dtype)
         out = F.relu(self.fc1(self.dropout(out)))
         out = F.relu(self.fc2(self.dropout(out)))
+        out = F.relu(self.fc3(self.dropout(out)))
         return out
-
+    
+    def getMasked(self, data, mask_len = 5):
+        mask = torch.ones_like(data)
+        _, _, seq_len = mask.shape
+        index = random.randint(0, seq_len - mask_len - 1)
+        mask[:,:,index:index + mask_len] = 0
+        data = data * mask
+        return data
