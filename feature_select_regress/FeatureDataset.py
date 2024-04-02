e21f8b4f0974a7e1a84e1c3cb4a6c44a97003d02
mhl34 <67287623+mhl34@users.noreply.github.com>
2024-04-02 09:18:27 -0400

    Update FeatureDataset.py

--- feature_select_regress/FeatureDataset.py
+++ feature_select_regress/FeatureDataset.py
@@ -6,7 +6,7 @@ import torch
 from utils import createGlucStats
 
 class FeatureDataset(Dataset):
-    def __init__(self, samples, glucose, eda, hr, temp, acc, food, minutes, hba1c, metric = "mean", dtype = torch.float64, seq_length = 28, normalize = False):
+    def __init__(self, samples, glucose, eda, hr, temp, acc, food, minutes, hba1c, metric = "mean", dtype = torch.float64, seq_length = 28, transforms = None):
         self.samples = samples
         self.glucose = glucose
         self.eda = eda
@@ -20,7 +20,7 @@ class FeatureDataset(Dataset):
         self.pp5vals = pp5()
         self.metric = metric
         self.dtype = dtype
-        self.normalize = normalize
+        self.transforms = transforms
 
     def __len__(self):
         return self.glucose[self.samples[0]].__len__() - self.seq_length + 1
@@ -34,7 +34,7 @@ class FeatureDataset(Dataset):
         # edaSample = self.eda[sample]
         # hrSample = self.hr[sample]
         # tempSample = self.temp[sample]
-        accSample = self.acc[sample]
+        # accSample = self.acc[sample]
         minSample = self.minutes[sample]
         hba1cSample = self.hba1c[sample]
 
@@ -43,16 +43,17 @@ class FeatureDataset(Dataset):
         
         # get the start indices for different sequences
         glucStart = sugarStart = carbStart = minStart = hba1cStart = index
-        accStart = glucStart * self.pp5vals.acc
+        # accStart = glucStart * self.pp5vals.acc
         
         glucTruth = self.truthCheck(glucoseSample, glucStart, "glucose")
         sugarTruth = self.truthCheck(sugarSample, glucStart, "gluc_other")
         carbTruth = self.truthCheck(carbSample, carbStart, "gluc_other")
         minTruth = self.truthCheck(minSample, minStart, "gluc_other")
         hba1cTruth = self.truthCheck(hba1cSample, hba1cStart, "gluc_other")
-        accTruth = self.truthCheck(accSample, accStart, "acc")
+        # accTruth = self.truthCheck(accSample, accStart, "acc")
         
-        while glucTruth or sugarTruth or carbTruth or minTruth or hba1cTruth or accTruth:
+        # while glucTruth or sugarTruth or carbTruth or minTruth or hba1cTruth or accTruth:
+        while glucTruth or sugarTruth or carbTruth or minTruth or hba1cTruth:
             idx = random.randint(0,len(glucoseSample) - 2 * self.seq_length - 1)
             glucStart = sugarStart = carbStart = minStart = hba1cStart = idx
             accStart = glucStart * self.pp5vals.acc
@@ -61,7 +62,7 @@ class FeatureDataset(Dataset):
             carbTruth = self.truthCheck(carbSample, carbStart, "gluc_other")
             minTruth = self.truthCheck(minSample, minStart, "gluc_other")
             hba1cTruth = self.truthCheck(hba1cSample, hba1cStart, "gluc_other")
-            accTruth = self.truthCheck(accSample, accStart, "acc")
+            # accTruth = self.truthCheck(accSample, accStart, "acc")
             
         glucosePastSec = glucoseSample[glucStart: glucStart + self.seq_length]
         glucoseSec = glucoseSample[glucStart + self.seq_length + 1: glucStart + 2 * self.seq_length + 1]
@@ -69,24 +70,27 @@ class FeatureDataset(Dataset):
         carbSec = carbSample[carbStart: carbStart + self.seq_length]
         minSec = minSample[minStart: minStart + self.seq_length]
         hba1cSec = hba1cSample[hba1cStart: hba1cStart + self.seq_length]
-        accSec = accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]
+        # accSec = accSample[accStart: accStart + self.seq_length * self.pp5vals.acc]
         # glucStats = createGlucStats(glucoseSec)
         
         # create averages across sequence length
-        accMean = np.array(list(map(np.mean, np.array_split(accSec, self.seq_length))))
-        sugarMean = np.array(list(map(np.mean, np.array_split(sugarSec, self.seq_length))))
-        carbMean = np.array(list(map(np.mean, np.array_split(carbSec, self.seq_length))))
-        minMean = np.array(list(map(np.mean, np.array_split(minSec, self.seq_length))))
-        hba1cMean = np.array(list(map(np.mean, np.array_split(hba1cSec, self.seq_length))))
-        glucPastMean = np.array(list(map(np.mean, np.array_split(glucosePastSec, self.seq_length))))
-        glucMean = np.array(list(map(np.mean, np.array_split(glucoseSec, self.seq_length))))
+        # accMean = np.array(list(map(np.mean, np.array_split(accSec, self.seq_length))))
+        sugarMean = torch.Tensor(np.array(list(map(np.mean, np.array_split(sugarSec, self.seq_length)))))
+        carbMean = torch.Tensor(np.array(list(map(np.mean, np.array_split(carbSec, self.seq_length)))))
+        minMean = torch.Tensor(np.array(list(map(np.mean, np.array_split(minSec, self.seq_length)))))
+        hba1cMean = torch.Tensor(np.array(list(map(np.mean, np.array_split(hba1cSec, self.seq_length)))))
+        glucPastMean = torch.Tensor(np.array(list(map(np.mean, np.array_split(glucosePastSec, self.seq_length)))))
+        glucMean = torch.Tensor(np.array(list(map(np.mean, np.array_split(glucoseSec, self.seq_length)))))
 
         # normalize if needed
-        if self.normalize:
-            return (sample, self.normalizeFn(accMean), self.normalizeFn(sugarMean), self.normalizeFn(carbMean), self.normalizeFn(minMean), self.normalizeFn(hba1cMean), self.normalizeFn(glucPastMean), self.normalizeFn(glucMean))
+        output = torch.stack((sugarMean, carbMean, minMean, hba1cMean, glucPastMean, glucMean)).unsqueeze(1)
+        if self.transforms != None:
+            # return (sample, self.normalizeFn(accMean), self.normalizeFn(sugarMean), self.normalizeFn(carbMean), self.normalizeFn(minMean), self.normalizeFn(hba1cMean), self.normalizeFn(glucPastMean), self.normalizeFn(glucMean))
+            output = self.transforms(output)
         
         #return non-normalized outputs
-        return (sample, accMean, sugarMean, carbMean, minMean, hba1cMean, glucPastMean, glucMean)
+        # return (sample, accMean, sugarMean, carbMean, minMean, hba1cMean, glucPastMean, glucMean)
+        return output.to(self.dtype)
     
     def normalizeFn(self, data, eps = 1e-5):
         data = data[~np.isnan(data)]
